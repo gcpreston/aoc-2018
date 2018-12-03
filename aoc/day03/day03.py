462411789b7dae333a40ce0358ad29c96d381101
Graham Preston <preston.gr@husky.neu.edu>
2018-12-03 12:01:03 -0500

    Add day 3

---
+++ aoc/day03/day03.py
@@ -0,0 +1,54 @@
+import re
+
+
+def main():
+    p = re.compile('#(\d+) @ (\d+),(\d+): (\d+)x(\d+)')
+    with open('input.txt') as file:
+        claims = []
+        for line in file.readlines():
+            m = re.search(p, line.strip())
+            claims.append((int(m.group(1)), int(m.group(2)), int(m.group(3)), int(m.group(4)), int(m.group(5))))
+
+    ans1, ans2 = solve(claims)
+    print('Part 1:', ans1)
+    print('Part 2:', ans2)
+
+
+def solve(claims):
+    fabric = [['.'] * 1000 for _ in range(1000)]
+    no_overlap = set()
+    count = 0
+
+    for claim in claims:
+        no_overlap.add(claim[0])
+
+        for r in range(claim[4]):
+            for c in range(claim[3]):
+                row = claim[2] + r
+                col = claim[1] + c
+                f = fabric[row][col]
+
+                if type(f) is int:
+                    # already been visited exactly once
+                    if f in no_overlap:
+                        no_overlap.remove(f)
+                    if claim[0] in no_overlap:
+                        no_overlap.remove(claim[0])
+                    count += 1
+                    fabric[row][col] = 'X'
+                elif f == '.':
+                    fabric[row][col] = claim[0]
+
+    return count, no_overlap.pop()
+
+
+def part1(claims):
+    return solve(claims)[0]
+
+
+def part2(claims):
+    return solve(claims)[1]
+
+
+if __name__ == '__main__':
+    main()
---
+++ aoc/day03/day03_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from aoc.day03 import day03
+
+
+class TestDay03(unittest.TestCase):
+
+    def test_part1(self):
+        claims = [(1, 3, 2, 5, 4)]
+        self.assertEqual(0, day03.part1(claims))
+
+        claims = [(1, 1, 3, 4, 4),
+                  (1, 3, 1, 4, 4),
+                  (1, 5, 5, 2, 2)]
+        self.assertEqual(4, day03.part1(claims))
+
+
+if __name__ == '__main__':
+    unittest.main()
