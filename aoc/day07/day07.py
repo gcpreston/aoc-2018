60f9587121b92a1945e6b8daa7151271921b4991
Graham Preston <preston.gr@husky.neu.edu>
2018-12-13 12:43:28 -0500

    Finish day 7

--- aoc/day07/day07.py
+++ aoc/day07/day07.py
@@ -10,10 +10,6 @@ def main():
             m = p.match(line.strip())
             edges.append((m.group(1), m.group(2)))
 
-    print('Part 1:', part1(edges))
-
-
-def part1(edges):
     g = DirectedGraph()
     added = set()
 
@@ -27,11 +23,56 @@ def part1(edges):
 
         g.add_edge(u, v)
 
+    print('Part 1:', part1(g))
+    print('Part 2:', part2(g))
+
+
+def part1(g: DirectedGraph):
     return ''.join(g.topological_sort())
 
 
-def part2():
-    pass
+def part2(g: DirectedGraph, n=5):
+    # TODO: Implement using library dfs
+    clock = 0
+    # subtract 64 for character value and add 60 flat gives -4
+    tasks = {v: ord(v) - 4 for v in g.nodes()}
+    # how many remaining prerequesites does each task have?
+    in_degrees = {v: len(g.in_edges(v)) for v in g.nodes()}
+    to_be_removed = []
+
+    def assign_workers():
+        while len(workers) < n and queue:
+            workers.append(
+                queue.pop(min((queue[i], i) for i in range(len(queue)))[1]))
+
+    queue = [v for v in g.nodes() if len(g.in_edges(v)) == 0]
+    workers = []
+    assign_workers()
+
+    while workers:
+        for u in workers:
+            tasks[u] -= 1
+
+            if tasks[u] == 0:
+                to_be_removed.append(u)
+
+                for _, v in g.out_edges(u):
+                    in_degrees[v] -= 1
+
+                    # check if all prerequisites have been completed
+                    if in_degrees[v] == 0:
+                        queue.append(v)
+
+        # remove from workers while not iterating over list to avoid bugs
+        while to_be_removed:
+            workers.remove(to_be_removed.pop())
+
+        # re-populate workers
+        assign_workers()
+
+        clock += 1
+
+    return clock
 
 
 if __name__ == '__main__':
---
+++ aoc/day07/day07_test.py
@@ -0,0 +1,21 @@
+import unittest
+
+from data.graphs import DirectedGraph
+from aoc.day07 import day07
+
+
+class TestDay07(unittest.TestCase):
+
+    def test_part2(self):
+        g = DirectedGraph()
+        g.add_nodes('A', 'B', 'C', 'D', 'E', 'F')
+
+        g.add_edge('C', 'A')
+        g.add_edge('C', 'F')
+        g.add_edge('A', 'B')
+        g.add_edge('A', 'D')
+        g.add_edge('B', 'E')
+        g.add_edge('D', 'E')
+        g.add_edge('F', 'E')
+
+        self.assertEqual(258, day07.part2(g, n=2))
